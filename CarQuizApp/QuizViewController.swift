91613eb754e590597f4d5ac4f8e9a3b092ddd46b
Mayachiu <mayachiu5g@gmail.com>
2021-10-01 03:28:13 +0900

    quiz取得

--- CarQuizApp/QuizViewController.swift
+++ CarQuizApp/QuizViewController.swift
@@ -45,7 +45,7 @@ class QuizViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let scoreVC = segue.destination as! ScoreViewController
         scoreVC.correct = correctCount
-        scoreVC.cellCount = csvArray.count
+       
         scoreVC.scoreCSVArray = csvArray
         scoreVC.scoreQuizArray = quizArray
         
--- CarQuizApp/ScoreViewController.swift
+++ CarQuizApp/ScoreViewController.swift
@@ -14,39 +14,45 @@ class ScoreViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var tableView: UITableView!
     
     var correct = 0
-    var cellCount = 0
+    
     var scoreCSVArray:[String] = []
     var scoreQuizArray:[String] = []
     
+    var scoreQuizNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
+        scoreQuizNumber = scoreCSVArray.count
 
         scoreLabel.text = "\(correct)問正解！"
       
         print(scoreCSVArray)
+        
+        print(scoreCSVArray.count)
+        
+        
         print(scoreQuizArray)
         // Do any additional setup after loading the view.
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return cellCount
-    }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return scoreQuizNumber
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! MainTableViewCell
         cell.scoreAnswerLabel.text = "正解は○です"
+//        scoreQuizArray = scoreCSVArray[scoreQuizNumber - 1].components(separatedBy: ",")
+        scoreQuizArray = [scoreCSVArray[indexPath.row]]
+        print(scoreQuizArray.count)
+        print("aa")
         cell.scoreQuizTextView.text = scoreQuizArray[0]
         return cell
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "\(section + 1)問"
+        return "解答"
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
