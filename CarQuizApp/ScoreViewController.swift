9737246ffdb3cac4085b76b275fd3ab267b3fb88
Mayachiu <mayachiu5g@gmail.com>
2021-10-01 22:08:26 +0900

    ボタン太字

--- CarQuizApp/ScoreViewController.swift
+++ CarQuizApp/ScoreViewController.swift
@@ -99,6 +99,15 @@ class ScoreViewController: UIViewController, UITableViewDelegate, UITableViewDat
         self.presentingViewController?.presentingViewController?.dismiss(animated: true)
         
     }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+            // 背景色を変更する
+            view.tintColor = UIColor(hex: "F3F0D7")
+        }
+
+    
+ 
+    
     /*
     // MARK: - Navigation
 
@@ -109,4 +118,14 @@ class ScoreViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     */
 
+}
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let v = Int("000000" + hex, radix: 16) ?? 0
+        let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+        let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+        let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+    }
 }
